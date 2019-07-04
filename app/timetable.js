9b98980ee5ba75c74bab97a0ffc1940b764b1c90
Martin Komischke <Martin.Komischke@quentic.com>
2019-07-04 15:26:07 +0200

    test if the Weekdays are present in Toni's timetable

--- app/timetable.js
+++ app/timetable.js
@@ -1,6 +1,20 @@
-const fs = require('fs');
+const toni = require('../data/toni.json');
+const jannis_a = require('../data/jannis-a.json');
+const jannis_b = require('../data/jannis-b.json');
+const moritz = require('../data/moritz.json');
 
-exports.fromJson = function(path) {
-    const raw = fs.readFileSync(path);
-    return JSON.parse(raw);
+exports.toni = function() {
+    return toni;
+}
+
+exports.jannis_a = function() {
+    return jannis_a;
+}
+
+exports.jannis_b = function() {
+    return jannis_b;
+}
+
+exports.moritz = function() {
+    return moritz;
 }
--- test/timetable.js
+++ test/timetable.js
@@ -4,10 +4,8 @@ const timetable = require("../app/timetable");
 describe("Timetable", function() {
   describe("Reading JSON for Toni should succed", function() {
     it("run", function() {
-      const path = "..\data\toni.json";
-    //   const table = fromJson(path);
-    //   expect(table).contains('foo');
-    expect(true).equal(true);
+      const table = timetable.toni();
+      expect(table).to.be.deep.property('Weekdays').not.null;
     });
   });
 });
