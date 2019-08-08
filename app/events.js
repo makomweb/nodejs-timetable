2aa15e2e7cefd12c52ef2ba1f5fe60ec72c5e155
Martin Komischke <Martin.Komischke@quentic.com>
2019-08-08 16:16:57 +0200

    better usage of blockstart times

--- app/events.js
+++ app/events.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const week = require('./week');
 const helper = require('./helper');
 
-exports.weekday = function(timetable, index) {    
+exports.weekday = function(timetable, index) {
+    const startTimes = helper.getNestedObject(timetable, ['BlockStartTimes']);
     const day = helper.getNestedObject(timetable, ['Weekdays'])[index];
     const blocks = helper.getNestedObject(day, ['Blocks']);
     const first = week.firstDayOfWeek(new Date(), 1); // now, 0 = Sunday, 1 = Monday is first day of a week
@@ -13,7 +14,7 @@ exports.weekday = function(timetable, index) {
         const year = date.getFullYear();
         const month = date.getMonth() + 1; // month counting starts at 0 (0 = January)
         const day = date.getDate();
-        const startString = `${year}, ${month}, ${day}, ${block.Begin}`;        
+        const startString = `${year}, ${month}, ${day}, ${startTimes[index]}`;        
         const start = new Date(startString);
         const end = new Date(start.getTime() + 2700000); // + 45 min
         const subject = block.Name;
