76f63e747d8dfdbe292973bbcf340cee08bf5458
Martin Komischke <Martin.Komischke@quentic.com>
2019-08-08 16:25:55 +0200

    fix test

--- test/week.js
+++ test/week.js
@@ -4,8 +4,8 @@ const week = require('../app/week');
 describe("Week", function() {
   describe("Determining first day of week should succeed", function() {
     it("run", function() {
-      const first = week.firstDayOfWeek(new Date(), 2);
-      expect(first).equal('foo');
+      const first = week.firstDayOfWeek(new Date(), 1);
+      expect(first.toString()).contains('Mon ');
     });
   });
 });
