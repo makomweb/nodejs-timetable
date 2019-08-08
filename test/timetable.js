b0625dfa9f9c3f43d1935dac3b1298de6d4105af
Martin Komischke <Martin.Komischke@quentic.com>
2019-08-08 16:25:42 +0200

    adjust tests

--- test/timetable.js
+++ test/timetable.js
@@ -3,15 +3,15 @@ const timetable = require('../app/timetable');
 const helper = require('../app/helper');
 
 describe("Timetable", function() {
-  describe("Reading JSON for Toni should succed", function() {
+  describe("Reading JSON for Achard 6C should succed", function() {
     it("run", function() {
-      const table = timetable.toni();
+      const table = timetable.achard_6_c();
       expect(table).to.be.deep.property('Weekdays').not.null;
     });
   });
-  describe("Getting Monday for Toni should succeed", function() {
+  describe("Getting Monday for Achard 6C should succeed", function() {
     it ("run", function() {
-      const table = timetable.toni();
+      const table = timetable.achard_6_c();
       const Monday = helper.getNestedObject(table, ['Weekdays'])[0];
       expect(Monday).to.be.deep.property('Name').equal('Monday');
     });
